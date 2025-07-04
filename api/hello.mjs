1906dd10d95512f024cebe41551908ee347c3bfd
arka1002 <arka.mcu700@gmail.com>
2025-07-04 21:26:47 +0530

    121

--- api/hello.mjs
+++ api/hello.mjs
@@ -1,5 +1,5 @@
 export async function GET(request) {
   const response = await fetch('https://api.vercel.app/products');
   const products = await response.json();
-  return Response.json(products);
+  return new Response("hello mom");
 }
