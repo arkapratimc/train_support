33161bd088550225561b0f592fb1225e554c6dd4
arka1002 <arka.mcu700@gmail.com>
2025-07-05 17:29:08 +0530

    added puppeteer, lets see now

---
+++ api/puppet.mjs
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer-core';
+
+export async function GET(request) {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    const title = await page.title();
+    await browser.close();
+
+    return new Response(title);
+}
