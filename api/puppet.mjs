28d685a241ba12c3d4ed6474623190adadfe72a6
arka1002 <arka.mcu700@gmail.com>
2025-07-05 17:32:00 +0530

    executable path added

--- api/puppet.mjs
+++ api/puppet.mjs
@@ -1,7 +1,9 @@
-import puppeteer from 'puppeteer-core';
+import puppeteer, {executablePath} from 'puppeteer-core';
 
 export async function GET(request) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        executablePath: executablePath()
+    });
 
     const page = await browser.newPage();
     await page.goto('https://example.com');
