95f7efc3744dc10f05f7197cd2f10fdf9275d9ac
arka1002 <arka.mcu700@gmail.com>
2025-07-06 10:32:14 +0530

    added initial code, lets see

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// hello-server.js
 const http = require('http');
+const fs = require('fs');
 const puppeteer = require("puppeteer");
 
 async function test() {
@@ -33,10 +33,30 @@ async function test() {
 }
 test().catch(err => console.error(err))
 
+function serveFile(filePath, contentType, res) {
+    fs.readFile(path.join(__dirname, filePath), (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('500 Server Error');
+        }
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
+
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;                // Set HTTP status code
-    res.setHeader('Content-Type', 'text/plain'); // Set response header
-    res.end('Hello, World!\n');          // Send response
+    // Simple routing
+    if (req.url === '/' || req.url === '/index.html') {
+        serveFile('public/index.html', 'text/html', res);
+    } else if (req.url === '/style.css') {
+        serveFile('public/style.css', 'text/css', res);
+    } else if (req.url === '/script.js') {
+        serveFile('public/script.js', 'text/javascript', res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
 });
 
 const port = 3000;
